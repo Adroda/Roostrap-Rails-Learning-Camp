da25428383b824fa078e2c395709da08c27eac8d
adroda <adrianchi_91@hotmail.com>
2023-05-12 14:04:41 -0300

    changed label > name & icon > image

--- db/migrate/20230511192831_create_topics.rb
+++ db/migrate/20230511192831_create_topics.rb
@@ -1,8 +1,8 @@
 class CreateTopics < ActiveRecord::Migration[7.0]
   def change
     create_table :topics do |t|
-      t.string :label
-      t.string :icon
+      t.string :name
+      t.string :image
 
       t.timestamps
     end
--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -3,14 +3,14 @@
 # Table name: topics
 #
 #  id         :bigint           not null, primary key
-#  label      :string
-#  icon       :string
+#  name      :string
+#  image       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 FactoryBot.define do
   factory :topic do
-    label { "MyString" }
-    icon { "MyString" }
+    name { "MyString" }
+    image { "MyString" }
   end
 end
