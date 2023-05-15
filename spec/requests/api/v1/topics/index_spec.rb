13afccc5eaac3965fe53e734a455e28af06a8de2
adroda <adrianchi_91@hotmail.com>
2023-05-15 16:49:18 -0300

    de todo un poco

--- spec/requests/api/v1/topics/index_spec.rb
+++ spec/requests/api/v1/topics/index_spec.rb
@@ -3,17 +3,21 @@
 # Table name: topics
 #
 #  id         :bigint           not null, primary key
-#  name      :string
-#  image       :string
+#  name       :string
+#  image      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 require 'rails_helper'
 
 RSpec.describe 'Topics', type: :request do
+  subject { get api_v1_topics_path, headers: auth_headers, as: :json }
+
+  let(:user) { create(:user) }
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/topics/index'
+      subject
       expect(response).to have_http_status(:success)
     end
   end
