3b27178f5770d07d16844f0375729208894920c9
adroda <adrianchi_91@hotmail.com>
2023-05-18 15:03:15 -0300

    still fixing

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class TopicsController < Api::V1::TopicsController
+    class TopicsController < Api::V1::ApiController
       def index
         @topics = policy_scope(Topic)
       end
