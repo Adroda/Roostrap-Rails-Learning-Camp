d796666ac987ae24365ca8a1b893af6445c77295
adroda <adrianchi_91@hotmail.com>
2023-05-18 16:03:53 -0300

    still fixing

--- spec/routing/topic_routing_spec.rb
+++ spec/routing/topic_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TopicsController, type: :routing do
+RSpec.describe API::V1::TopicsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/topics').to route_to('topics#index')
