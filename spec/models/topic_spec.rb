2a71213d0aec28e846af76d68a4d3fba7d678ef3
adroda <adrianchi_91@hotmail.com>
2023-05-19 19:17:04 -0300

    tests running

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,12 +8,11 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-require 'rails_helper'
 
-RSpec.describe Topic, type: :model do
-  describe 'validations' do
-    subject { build :topic }
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:image) }
-  end
+describe Topic do
+  subject(:topic) { FactoryBot.create(:topic) }
+  it { is_expected.to be_an Topic }
+  it { is_expected.to be_persisted }
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:image).of_type(:string) }
 end
--- spec/routing/topic_routing_spec.rb
+++ spec/routing/topic_routing_spec.rb
@@ -1,13 +1,7 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::TopicsController, type: :routing do
+describe Api::V1::UsersController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/topics').to route_to('topics#index')
-    end
-
-    it 'routes to #create' do
-      expect(post: '/topics').to route_to('topics#create')
+      expect(get: '/api/v1/topics').to route_to('api/v1/topics#index', format: :json)
     end
   end
 end
