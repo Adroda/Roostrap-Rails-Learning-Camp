8af1de54c4e8b330b6bfea3e7ec305a33a56919d
adroda <adrianchi_91@hotmail.com>
2023-05-29 15:36:12 -0300

    validation for topic attributes being sent as well

--- spec/requests/api/v1/topics/index_spec.rb
+++ spec/requests/api/v1/topics/index_spec.rb
@@ -1,24 +1,25 @@
-# == Schema Information
-#
-# Table name: topics
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  image      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-require 'rails_helper'
-
-RSpec.describe 'Topics', type: :request do
+describe 'GET api/v1/topics', type: :request do
+  let(:user)          { create(:user) }
+  let!(:topics)       { create_list(:topic, 10) }
+  let(:first_topic)   { topics.first }
   subject { get api_v1_topics_path, headers: auth_headers, as: :json }
 
-  let(:user) { create(:user) }
+  it 'returns a successful response' do
+    subject
+    expect(response).to be_successful
+  end
 
-  describe 'GET /index' do
-    it 'returns http success' do
-      subject
-      expect(response).to have_http_status(:success)
-    end
+  it 'returns all topics' do
+    subject
+    expect(json[:topics].pluck(:name)).to match_array(topics.pluck(:name))
+  end
+
+  it 'returns topics in correct format' do
+    subject
+    expect(json[:topics].first[:id]).to eq(first_topic.id)
+    expect(json[:topics].first[:name]).to eq(first_topic.name)
+    expect(json[:topics].first[:image]).to eq(first_topic.image)
+    expect(json[:topics].first[:created_at]).to eq(first_topic.created_at.as_json)
+    expect(json[:topics].first[:updated_at]).to eq(first_topic.updated_at.as_json)
   end
 end
